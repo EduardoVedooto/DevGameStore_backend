2c124c87198ded5749f364b3ef9309074a43aabb
Guga <contato.gustavobarbosa@outlook.com>
2021-07-15 14:42:03 -0300

    Preparando revisão do front-end da D4

--- src/Server.js
+++
@@ -1,4 +0,0 @@
-import './setup.js';
-import app from "./App.js";
-
-app.listen(process.env.PORT, () => console.info(`Server running on port ${process.env.PORT}...`));
--- src/sanitization/card.js
+++
@@ -1,9 +0,0 @@
-const cardSanitization = data => {
-  const { name } = data;
-  return {
-    ...data,
-    name: name.split(" ").filter(w => w).join(" ")
-  }
-}
-
-export default cardSanitization;
